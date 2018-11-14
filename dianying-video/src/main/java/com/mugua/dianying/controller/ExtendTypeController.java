0df0b4bc57d7b1122dca0496bf7f5b4398d406db
wangshuangchao <286958067@qq.com>
2018-11-15 07:43:55 +0800

    优化了ExtendTypeController的修改方法

--- dianying-video/src/main/java/com/mugua/dianying/controller/ExtendTypeController.java
+++ dianying-video/src/main/java/com/mugua/dianying/controller/ExtendTypeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
@@ -15,7 +14,6 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.mugua.dianying.common.constant.CommonConstant;
 import com.mugua.dianying.common.pojo.RespBean;
 import com.mugua.dianying.common.utils.ObjectId;
-import com.mugua.dianying.entity.Board;
 import com.mugua.dianying.entity.ExtendType;
 import com.mugua.dianying.entity.ValScope;
 import com.mugua.dianying.service.BoardService;
@@ -40,7 +38,7 @@ public class ExtendTypeController {
 	ExtendTypeService extendTypeService;
 	@Autowired
 	ValScopeService valScopeService;
-	
+
 	/**
 	 *  添加扩展属性
 	 * @param extendType 添加的扩展属性
@@ -49,7 +47,7 @@ public class ExtendTypeController {
 	@PostMapping("/extendtype")
 	public RespBean add(@RequestBody ExtendType extendType){
 		RespBean result = new RespBean();
-		
+
 		// 如果扩展属性名为空字符串或者属性不在范围之内,则不允许添加,
 		Integer typeVal = extendType.getExtendType();
 		boolean bo = typeVal == null || typeVal != 0 && typeVal != 1 && typeVal != 2 && typeVal !=3; 
@@ -58,23 +56,23 @@ public class ExtendTypeController {
 			result.setMsg("FAULT");
 			return result;
 		}
-		
+
 		// 添加id
 		String sysId = ObjectId.getObjectId();
 		extendType.setSysId(sysId);
-		
+
 		// 设置属性 0,单选 1,多选 2,数值 3,字符串
 		extendType.setExtendType(extendType.getExtendType());
 		extendType.setCreateTime(new Date());
-		
+
 		// 设置创建人(当前登录的操作员),创建时间 
 		extendType.setCreateMan("张三");
 		extendType.setModifyTime(new Date());
 		System.out.println(new Date());
-		
+
 		// 设置修改人(当前登录的操作员),修改时间
 		extendType.setModifyMan("张三");
-		
+
 		// 添加扩展属性
 		boolean flag = extendTypeService.insert(extendType);
 		if (flag) {
@@ -86,7 +84,7 @@ public class ExtendTypeController {
 		}
 		return result;
 	}
-	
+
 	/**
 	 *  根据id删除某个扩展属性(软删除)
 	 * @param id 扩展属性id
@@ -95,35 +93,34 @@ public class ExtendTypeController {
 	@DeleteMapping("/extendtype/{sysId}")
 	public RespBean delete(@PathVariable(name = "sysId") String id){
 		RespBean result = new RespBean();
-		
+
 		// 如果扩展属性已有可选值,不允许删除
-		EntityWrapper wrapperValScope = new EntityWrapper();
-		List<ValScope> data = valScopeService.selectList(wrapperValScope.eq("extend_type", id));
+		EntityWrapper<ValScope> wrapperValScope = new EntityWrapper<ValScope>();
+		wrapperValScope.eq("extend_type", id);
+		List<ValScope> data = valScopeService.selectList(wrapperValScope);
 		if (data.size() != 0) {
 			result.setStatus(500);
 			result.setMsg("FAULT");
 			return result;
 		}
-		
+
 		// 如果扩展属性不存在或者已经软删除,不允许删除
 		ExtendType extendType = extendTypeService.selectById(id);
-		
-		// 返回接口,是否增加成功
 		if (extendType == null || extendType.getIsDelete() ==1) {
 			result.setStatus(500);
 			result.setMsg("FAULT");
 			return result;
 		}
-		
+
 		// 设置变动信息: 修改人,修改时间
 		extendType.setModifyMan("张三");
 		extendType.setModifyTime(new Date());
-		
+
 		// 进行软删除
 		extendType.setIsDelete(CommonConstant.DELETE);
-		EntityWrapper wrapperExtendType = new EntityWrapper();
+		EntityWrapper<ExtendType> wrapperExtendType = new EntityWrapper<ExtendType>();
 		boolean flag = extendTypeService.update(extendType, wrapperExtendType.eq("sys_id", id));
-		
+
 		// 返回结果,是否(软)删除成功
 		if (flag) {
 			result.setStatus(200);
@@ -134,7 +131,7 @@ public class ExtendTypeController {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 修改扩展属性的名称或类型(单选,多选...)
 	 * @param extendType 要改变的扩展属性
@@ -143,15 +140,24 @@ public class ExtendTypeController {
 	@PutMapping("/extendtype")
 	public RespBean modify(@RequestBody ExtendType extendType){
 		RespBean result = new RespBean();
-		
+
+		// 如果扩展属性不存在或者已经软删除,不允许修改
+		String id = extendType.getSysId();
+		ExtendType extendTypeQue = extendTypeService.selectById(id);
+		if (extendTypeQue == null || extendTypeQue.getIsDelete() == 1) {
+			result.setStatus(500);
+			result.setMsg("FAULT");
+			return result;
+		}
+
 		// 设置修改人,修改时间
 		extendType.setModifyMan("张三");
 		extendType.setModifyTime(new Date());
 		System.out.println("board:" + extendType.toString());
-		
+
 		// 进行修改
 		boolean flag = extendTypeService.updateById(extendType);
-		
+
 		// 返回修改结果
 		if (flag) {
 			result.setStatus(200);
@@ -162,7 +168,7 @@ public class ExtendTypeController {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 根据id查询某个扩展属性信息
 	 * @param id
@@ -171,23 +177,22 @@ public class ExtendTypeController {
 	@GetMapping( "/extendtype/{sysId}")
 	public RespBean queryOneById(@PathVariable(name="sysId") String id){
 		RespBean result = new RespBean();
-		
-		// 根据id查询扩展属性信息
+
+		// 如果扩展属性不存在或者已经软删除,不允许查询
 		ExtendType extendType = extendTypeService.selectById(id);
-		
-		// 返回结果
-		if (extendType != null) {
-			result.setStatus(200);
-			result.setMsg("SUCCESS");
-			result.setObj(extendType);
-		}else {
+		if (extendType == null || extendType.getIsDelete() ==1) {
 			result.setStatus(500);
 			result.setMsg("FAULT");
+			return result;
 		}
+
+		result.setStatus(200);
+		result.setMsg("SUCCESS");
+		result.setObj(extendType);
 		return result;
-		
+
 	}
-	
+
 	/**
 	 * 分页查询扩展属性或根据版块查询扩展属性
 	 * @param current 当前页
@@ -206,10 +211,10 @@ public class ExtendTypeController {
 			page.setCurrent(Integer.parseInt(current));
 			page.setSize(Integer.parseInt(size));
 			EntityWrapper<ExtendType> wrapper = new EntityWrapper<ExtendType>();
-			
+
 			// 如果接收了版块,则根据版块进行筛选
 			if (boardSysId != null) {
-				
+
 				// 版块不存在,则查询失败
 				if (boardService.selectById(boardSysId) != null){
 					wrapper.eq("board", boardSysId);
@@ -221,9 +226,9 @@ public class ExtendTypeController {
 			}
 			Page<ExtendType> resultPage = extendTypeService.selectPage(page, wrapper.eq("is_delete", 0));
 			List<ExtendType> data = resultPage.getRecords();
-			
-			//表中总的数据条数
-			int total = resultPage.getTotal();
+
+			// 表中总的数据条数
+			// int total = resultPage.getTotal();
 			if (data != null) {
 				result.setStatus(200);
 				result.setMsg("SUCCESS");
